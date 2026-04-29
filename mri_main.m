798789c89e029ce78647793917daf7c71eb14a1f
jliljefors <johan@liljefors.eu>
2026-04-29 13:36:36 +0200

    Update mri_main.m

--- mri_main.m
+++ mri_main.m
@@ -2,6 +2,14 @@
 clear all
 close all
 restoredefaultpath
+addpath("C:\Github\WM_Distractors_OPM\Matlab")
+
+%% Set up fieldtrip
+
+A_Initialize_environment_OPM
+global STATIC;
+STATIC = StaticVarClass_WM_shift_OPM();
+ft_default.showcallinfo = 'no';
 
 %% Base paths
 if contains(pwd,'/home/chrpfe')
@@ -12,55 +20,50 @@ if contains(pwd,'/home/chrpfe')
     project_scripts_path = '/home/chrpfe/Documents/MATLAB/21099_opm/phalanges';
     on_server = true;
 else
-    % Laptop:
-    base_data_path = '/Volumes/dataarchvie/21099_opm';
-    base_save_path = '/Users/christophpfeiffer/data_local/Benchmarking/';
-    base_matlab_path = '/Users/christophpfeiffer/Dropbox/Mac/Documents/MATLAB';
-    project_scripts_path = '/Users/christophpfeiffer/opm_phalanges';
     on_server = false;
+    base_data_path = 'G:\SV10_OPM_Distractor_Processing\SV10_RawData\MEG\raw';
+    base_save_path = 'G:\SV10_OPM_Distractor_Processing\ProcessedData_Natmeg';
+    base_matlab_path = 'C:\Github\WM_Distractors_OPM\Matlab';
+    project_scripts_path = 'C:\toolbox\opm_general';
 end
 
-%% Set up fieldtrip
-addpath(fullfile(base_matlab_path,'fieldtrip/')) % Fieldtrip path
-addpath(fullfile(base_matlab_path,'fieldtrip_private')) % Fieldtrip private functions
-addpath(project_scripts_path)
-ft_defaults
+params.src_density = '32'; % Sourcemodel density ('4', '8' or '32') = approximate number of sourmes per hemisphere
 
-global ft_default
-ft_default.showcallinfo = 'no';
 
 %% Subjects + dates
-subses = {'0005' '240208';
-    '0905' '240229';
-    '0916' '240320';
-    '0953' '241104';
-    '1096' '241022';
-    '1153' '240321';
-    '1167' '240425';
-    '1186' '240925';
-    '1190' '241023';
-    '1191' '241024';
-    '1193' '241029';
-    '1194' '241029';
-    '1195' '241030'};
-mri_files = {'00000001.dcm' 
-    '/mri/sub-15931_T1w.nii.gz'  
-    '/nifti/anat/sub-15985_T1w.nii.gz'};
+% subses = {'0005' '240208';
+%     '0905' '240229';
+%     '0916' '240320';
+%     '0953' '241104';
+%     '1096' '241022';
+%     '1153' '240321';
+%     '1167' '240425';
+%     '1186' '240925';
+%     '1190' '241023';
+%     '1191' '241024';
+%     '1193' '241029';
+%     '1194' '241029';
+%     '1195' '241030'};
+% mri_files = {'00000001.dcm' 
+%     '/mri/sub-15931_T1w.nii.gz'  
+%     '/nifti/anat/sub-15985_T1w.nii.gz'};
+% 
+% if on_server
+%     subs_to_run = 1:size(subses,1);
+% else
+%     subs_to_run = 2; %1:size(subses,1)
+% end
+% excl_subs = [1];
 
-if on_server
-    subs_to_run = 1:size(subses,1);
-else
-    subs_to_run = 2; %1:size(subses,1)
-end
-excl_subs = [1];
+subs_to_run = 1:height(STATIC.subjectData);
 
 %% Loop over subjects
-for i_sub = setdiff(subs_to_run,excl_subs)
+for i_sub = subs_to_run
     params.sub = ['sub_' num2str(i_sub,'%02d')];
 
-    %% Paths
-    raw_path = fullfile(base_data_path,'MEG',['NatMEG_' subses{i_sub,1}], subses{i_sub,2});
-    mri_path = fullfile(base_data_path,'MRI',['NatMEG_' subses{i_sub,1}]);
+    % % Paths
+    % raw_path = fullfile(base_data_path,'MEG',['NatMEG_' subses{i_sub,1}], subses{i_sub,2});
+    % mri_path = fullfile(base_data_path,'MRI',['NatMEG_' subses{i_sub,1}]);
     save_path_mri = fullfile(base_save_path,'MRI',params.sub);
     
     % Create folders if they do not exist yet
@@ -70,8 +73,8 @@ for i_sub = setdiff(subs_to_run,excl_subs)
     if ~exist(save_path_mri, 'dir')
         mkdir(save_path_mri)
     end
-    meg_file = fullfile(raw_path, 'meg', 'AudOddMEG_proc-tsss+corr98+mc+avgHead_meg.fif');
-    %mri_file = fullfile(mri_path, 'orig','001.mgz');
-    prepare_mri(mri_path,meg_file,save_path_mri);
+    meg_file = STATIC.subjectData.OPM_FilePath(i_sub);
+    wsl_path_subj = char(fullfile(STATIC.wsl_path,"WM_Distractors_OPM",string(STATIC.subjectID(i_sub))));
+    prepare_mri(wsl_path_subj,meg_file,save_path_mri,params);
     close all
 end
